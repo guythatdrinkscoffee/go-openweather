b2a1a832ecab3807264e8210059273fbb88e5eba
J. Manuel Zaragoza <jmanueldev@gmail.com>
2024-10-15 20:22:03 -0700

    Defined common api response models

---
+++ data.go
@@ -0,0 +1,35 @@
+package openweather
+
+type Coord struct {
+	Lat float64 `json:"lat"`
+	Lon float64 `json:"lon"`
+}
+
+type Weather struct {
+	ID          int64  `json:"id"`
+	Main        string `json:"main"`
+	Description string `json:"description"`
+}
+
+type Main struct {
+	Temp      float64 `json:"temp"`
+	FeelsLike float64 `json:"feels_like"`
+	TempMin   float64 `json:"temp_min"`
+	TempMax   float64 `json:"temp_max"`
+	Pressure  int     `json:"pressure"`
+	Humidity  int     `json:"humidity"`
+	SeaLevel  int     `json:"sea_level"`
+	GrndLevel int     `json:"grnd_level"`
+}
+
+type Wind struct {
+	Speed float64 `json:"speed"`
+	Deg   int     `json:"deg"`
+	Gust  float64 `json:"gust"`
+}
+
+type Sys struct {
+	Country string `json:"country"`
+	Sunrise int64  `json:"sunrise"`
+	Sunset  int64  `json:"sunset"`
+}
